0de93144be5743f23c80d308efae958092c272c4
tasnim2410 <tasnim.nefzi@esprit.tn>
2025-11-23 01:52:09 +0100

    changed page logo and tab title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "App Pre-Launch - Coming Soon",
-  description: "Register now for early access to our revolutionary delivery app",
+  title: "Saweg",
+  description: "Register now for early access to the Saweg delivery app",
+  icons: {
+    icon: "/images/logo.png",
+  },
 };
 
 export default function RootLayout({
